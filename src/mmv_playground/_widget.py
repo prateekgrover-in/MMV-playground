0d2dd1fb15d6184e09abcfaadfc8ffaa53706c5f
lampen <lampen@isas.de>
2024-11-15 17:17:40 +0100

    bug fix

--- src/mmv_playground/_widget.py
+++ src/mmv_playground/_widget.py
@@ -20,35 +20,35 @@ if TYPE_CHECKING:
 class IntensityWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setVisibility(False)
+        self.setVisible(False)
 
         # Layout and parameters for intensity normalization
-        vbox_layout = QVBoxLayout()
-        self.setLayout(vbox_layout)
+        layout = QVBoxLayout()
+        self.setLayout(layout)
 
         button1 = QPushButton('Button 1')
-        vbox_layout.addWidget(button1)
+        layout.addWidget(button1)
 
-        vbox_layout.addWidget(QLabel('Parameter 1'))
-        self.param11 = QLineEdit(self)
-        vbox_layout.addWidget(self.param1)
+        layout.addWidget(QLabel('Parameter 1'))
+        self.param1 = QLineEdit(self)
+        layout.addWidget(self.param1)
 
 
 class SmoothingWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setVisibility(False)
+        self.setVisible(False)
 
         # Layout and parameters for intensity normalization
-        vbox_layout = QVBoxLayout()
-        self.setLayout(vbox_layout)
+        layout = QVBoxLayout()
+        self.setLayout(layout)
 
         button1 = QPushButton('Button 1')
-        vbox_layout.addWidget(button1)
+        layout.addWidget(button1)
 
-        vbox_layout.addWidget(QLabel('Parameter 1'))
-        self.param11 = QLineEdit(self)
-        vbox_layout.addWidget(self.param1)
+        layout.addWidget(QLabel('Parameter 1'))
+        self.param1 = QLineEdit(self)
+        layout.addWidget(self.param1)
 
 
 class mmv_playground(QWidget):
@@ -56,48 +56,54 @@ class mmv_playground(QWidget):
         super().__init__()
         self.viewer = viewer
 
-        # define a scroll area inside the main widget
+        # Define a layout for the main widget
+        layout1 = QVBoxLayout()
+        self.setLayout(layout1)
+
+        # Define a scroll area inside the QVBoxLayout
         scroll_area = QScrollArea()
         scroll_area.setWidgetResizable(True)
-        self.addWidget(scroll_area)
+        layout1.addWidget(scroll_area)
 
-        # define a group box inside the scroll area
+        # Define a group box inside the scroll area
         group_box = QGroupBox('MMV-Playground')
-        vbox_layout = QVBoxLayout()
-        group_box.setLayout(vbox_layout)
+        layout2 = QVBoxLayout()
+        group_box.setLayout(layout2)
         scroll_area.setWidget(group_box)        
 
-        # add widgets to the group box
-        self.button1 = QPushButton('Intensity normalization')
-        self.button1.clicked.connect(self.toggle_intensity_widget)
-        vbox_layout.addWidget(self.button1)
+        # Add widgets to the group box
+        # Button 'intensity normalization'
+        self.btn_intensity = QPushButton('Intensity normalization')
+        self.btn_intensity.clicked.connect(self.toggle_intensity_widget)
+        layout2.addWidget(self.btn_intensity)
 
         # Intensity widget
         self.intensity_widget = IntensityWidget(self)
-        vbox_layout.addWidget(self.intensity_widget)
+        layout2.addWidget(self.intensity_widget)
 
-        self.button2 = QPushButton('Smoothing')
-        self.button2.clicked.connect(self.toggle_smoothing_widget)
-        vbox_layout.addWidget(self.button2)
+        # Button smoothing
+        self.btn_smoothing = QPushButton('Smoothing')
+        self.btn_smoothing.clicked.connect(self.toggle_smoothing_widget)
+        layout2.addWidget(self.btn_smoothing)
 
         # Smoothing widget
         self.smoothing_widget = SmoothingWidget(self)
-        vbox_layout.addWidget(self.smoothing_widget)
+        layout2.addWidget(self.smoothing_widget)
 
     def toggle_intensity_widget(self):
-        # switching the visibility of the intensity widget
+        # Switching the visibility of the intensity widget
         if self.intensity_widget.isVisible():
-            self.intensity_widget.setVisibility(False)
-            self.button1.setText('Intensity normalization')
+            self.intensity_widget.setVisible(False)
+            self.btn_intensity.setText('Intensity normalization')
         else:
-            self.intensity_widget.setVisibility(True)
-            self.button1.setText('Hide intensity normalization')
+            self.intensity_widget.setVisible(True)
+            self.btn_intensity.setText('Hide intensity normalization')
 
     def toggle_smoothing_widget(self):
-        # switching the visibility of the smoothing widget
+        # Switching the visibility of the smoothing widget
         if self.smoothing_widget.isVisible():
-            self.smoothing_widget.setVisibility(False)
-            self.button2.setText('Smoothing')
+            self.smoothing_widget.setVisible(False)
+            self.btn_smoothing.setText('Smoothing')
         else:
-            self.intensity_widget.setVisibility(True)
-            self.button1.setText('Hide smoothing')
+            self.smoothing_widget.setVisible(True)
+            self.btn_smoothing.setText('Hide smoothing')
