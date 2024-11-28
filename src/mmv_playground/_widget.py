a31b43aaf87456508c3efde80cd8465b202adfbb
Lampen <lampen@isas.de>
2024-11-28 16:10:35 +0100

    class BackgroundCorrection

--- src/mmv_playground/_widget.py
+++ src/mmv_playground/_widget.py
@@ -6,6 +6,7 @@ from typing import TYPE_CHECKING
 import numpy as np
 from scipy.ndimage import gaussian_filter
 import itk
+from skimage.morphology import white_tophat, disk
 import napari
 from qtpy.QtCore import Qt
 from qtpy.QtWidgets import (
@@ -25,7 +26,7 @@ if TYPE_CHECKING:
     import napari
 
 
-class IntensityGroup(QGroupBox):
+class IntensityNormalization(QGroupBox):
     # (15.11.2024)
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -38,7 +39,7 @@ class IntensityGroup(QGroupBox):
         self.parent = parent
         self.name = ''          # layer[name]
         self.lower_percentage = 0.0
-        self.upper_percentage = 100.0
+        self.upper_percentage = 95.0
 
         # layout and parameters for intensity normalization
         vbox = QVBoxLayout()
@@ -52,41 +53,41 @@ class IntensityGroup(QGroupBox):
 
         self.lbl_lower = QLabel('lower percentage')
         vbox.addWidget(self.lbl_lower)
-        lower = QSlider(Qt.Horizontal)
-        lower.setRange(0, 500)
-        lower.setSingleStep(1)
-        lower.valueChanged.connect(self.lower_changed)
-        vbox.addWidget(lower)
+        sld_lower = QSlider(Qt.Horizontal)
+        sld_lower.setRange(0, 500)
+        sld_lower.setSingleStep(1)
+        sld_lower.valueChanged.connect(self.sld_lower_changed)
+        vbox.addWidget(sld_lower)
 
         self.lbl_upper = QLabel('Upper percentage')
         vbox.addWidget(self.lbl_upper)
-        upper = QSlider(Qt.Horizontal)
-        upper.setRange(9500, 10000)
-        upper.setSingleStep(1)
-        upper.valueChanged.connect(self.upper_changed)
-        vbox.addWidget(upper)
+        sld_upper = QSlider(Qt.Horizontal)
+        sld_upper.setRange(9500, 10000)
+        sld_upper.setSingleStep(1)
+        sld_upper.valueChanged.connect(self.sld_upper_changed)
+        vbox.addWidget(sld_upper)
 
         btn_run = QPushButton('run')
-        btn_run.clicked.connect(self.run_intensity)
+        btn_run.clicked.connect(self.run_intensity_normalization)
         vbox.addWidget(btn_run)
 
     def cbx_image_changed(self, index: int):
         # (19.11.2024)
         self.name = self.parent.layer_names[index]
 
-    def lower_changed(self, value: int):
+    def sld_lower_changed(self, value: int):
         # (19.11.2024)
         self.lower_percentage = value / 100.0
         self.lbl_lower.setText('lower percentage: %.2f' % \
             (self.lower_percentage))
 
-    def upper_changed(self, value: int):
+    def sld_upper_changed(self, value: int):
         # (19.11.2024)
         self.upper_percentage = value / 100.0
         self.lbl_upper.setText('upper percentage: %.2f' % \
             (self.upper_percentage))
 
-    def run_intensity(self):
+    def run_intensity_normalization(self):
         # (22.11.2024)
         if any(layer.name == self.name for layer in self.viewer.layers):
             layer = self.viewer.layers[self.name]
@@ -102,7 +103,7 @@ class IntensityGroup(QGroupBox):
         self.viewer.add_image(output, name=self.name)
 
 
-class SmoothingGroup(QGroupBox):
+class Smoothing(QGroupBox):
     # (15.11.2024)
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -187,6 +188,64 @@ class SmoothingGroup(QGroupBox):
         self.viewer.add_image(output, name=self.name)
 
 
+class BackgroundCorrection(QGroupBox):
+    # (28.11.2024)
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setTitle('Background correction')
+        self.setVisible(False)
+        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+        self.setStyleSheet('QGroupBox {background-color: blue; ' \
+            'border-radius: 10px}')
+        self.viewer = parent.viewer
+        self.parent = parent
+        self.name = ''              # layer[name]
+        self.kernel_size = 0
+
+        # vbox and parameters for smoothing
+        vbox = QVBoxLayout()
+        self.setLayout(vbox)
+
+        vbox.addWidget(QLabel('Image'))
+        self.cbx_image = QComboBox()
+        self.cbx_image.addItems(parent.layer_names)
+        self.cbx_image.currentIndexChanged.connect(self.cbx_image_changed)
+        vbox.addWidget(self.cbx_image)
+
+        self.lbl_kernel = QLabel('Kernel size')
+        vbox.addWidget(self.lbl_kernel)
+        sld_kernel = QSlider(Qt.Horizontal)
+        sld_kernel.setRange(0, 100)
+        sld_kernel.setSingleStep(1)
+        sld_kernel.valueChanged.connect(self.sld_kernel_changed)
+        vbox.addWidget(sld_kernel)
+
+        btn_run = QPushButton('run')
+        btn_run.clicked.connect(self.run_background_correction)
+        vbox.addWidget(btn_run)
+
+    def cbx_image_changed(self, index: int):
+        # (19.11.2024)
+        self.name = self.parent.layer_names[index]
+
+    def sld_kernel_changed(self, value: int):
+        # (28.11.2024)
+        self.kernel_size = value
+        self.lbl_kernel.setText('Kernel size: %d' % (value))
+
+    def run_background_correction(self):
+        # (28.11.2024)
+        if any(layer.name == self.name for layer in self.viewer.layers):
+            layer = self.viewer.layers[self.name]
+            input_image = layer.data
+        else:
+            print('Error: The image %s don\'t exist!' % (self.name))
+            return
+
+        output = white_tophat(input_image, disk(self.kernel_size))
+        self.viewer.add_image(output, name=self.name)
+
+
 class mmv_playground(QWidget):
     # (15.11.2024)
     def __init__(self, viewer: "napari.viewer.Viewer"):
@@ -214,54 +273,58 @@ class mmv_playground(QWidget):
         scroll_area.setWidget(group_box)        
 
         # Button intensity normalization
-        btn_intensity = QPushButton('Intensity normalization')
-        btn_intensity.setCheckable(True)
-        btn_intensity.clicked.connect(self.toggle_intensity_group)
-        vbox2.addWidget(btn_intensity)
+        self.btn_intensity = QPushButton('Intensity normalization')
+        self.btn_intensity.setCheckable(True)
+        self.btn_intensity.clicked.connect(self.toggle_intensity_normalization)
+        vbox2.addWidget(self.btn_intensity)
 
         # Intensity normalization group
-        self.intensity_group = IntensityGroup(self)
-        vbox2.addWidget(self.intensity_group)
+        self.intensity_normalization = IntensityNormalization(self)
+        vbox2.addWidget(self.intensity_normalization)
 
         # Button smoothing
-        btn_smoothing = QPushButton('Smoothing')
-        btn_smoothing.setCheckable(True)
-        btn_smoothing.clicked.connect(self.toggle_smoothing_group)
-        vbox2.addWidget(btn_smoothing)
+        self.btn_smoothing = QPushButton('Smoothing')
+        self.btn_smoothing.setCheckable(True)
+        self.btn_smoothing.clicked.connect(self.toggle_smoothing)
+        vbox2.addWidget(self.btn_smoothing)
 
-        # Smoothing widget
-        self.smoothing_group = SmoothingGroup(self)
-        vbox2.addWidget(self.smoothing_group)
+        # Smoothing group
+        self.smoothing = Smoothing(self)
+        vbox2.addWidget(self.smoothing)
 
         # Button background correction
-        btn_background = QPushButton('Background correction')
-        btn_background.setCheckable(True)
-        # btn_background.clicked.connect(self.toggle_background_group)
-        vbox2.addWidget(btn_background)
+        self.btn_background = QPushButton('Background correction')
+        self.btn_background.setCheckable(True)
+        self.btn_background.clicked.connect(self.toggle_background_correction)
+        vbox2.addWidget(self.btn_background)
+
+        # Background group
+        self.background_correction = BackgroundCorrection(self)
+        vbox2.addWidget(self.background_correction)
 
         # Button spot-shape filter
-        btn_spot_shape = QPushButton('Spot-shape filter')
-        btn_spot_shape.setCheckable(True)
-        # btn_spot_shape.clicked.connect(self.toggle_spot_shape_group)
-        vbox2.addWidget(btn_spot_shape)
+        self.btn_spot_shape = QPushButton('Spot-shape filter')
+        self.btn_spot_shape.setCheckable(True)
+        # self.btn_spot_shape.clicked.connect(self.toggle_spot_shape_group)
+        vbox2.addWidget(self.btn_spot_shape)
 
         # Button filament-shape filter
-        btn_filament = QPushButton('Filament-shape filter')
-        btn_filament.setCheckable(True)
-        # btn_filament.clicked.connect(self.toggle_filament_group)
-        vbox2.addWidget(btn_filament)
+        self.btn_filament = QPushButton('Filament-shape filter')
+        self.btn_filament.setCheckable(True)
+        # self.btn_filament.clicked.connect(self.toggle_filament_group)
+        vbox2.addWidget(self.btn_filament)
 
         # Button thresholding
-        btn_thresholding = QPushButton('Thresholding')
-        btn_thresholding.setCheckable(True)
-        # btn_thresholding.clicked.connect(self.toggle_thresholding_group)
-        vbox2.addWidget(btn_thresholding)
+        self.btn_thresholding = QPushButton('Thresholding')
+        self.btn_thresholding.setCheckable(True)
+        # self.btn_thresholding.clicked.connect(self.toggle_thresholding_group)
+        vbox2.addWidget(self.btn_thresholding)
 
         # Button topology-preserving thinning
-        btn_topology = QPushButton('Topology-preserving thinning')
-        btn_topology.setCheckable(True)
-        # btn_topology.clicked.connect(self.toggle_topology_group)
-        vbox2.addWidget(btn_topology)
+        self.btn_topology = QPushButton('Topology-preserving thinning')
+        self.btn_topology.setCheckable(True)
+        # self.btn_topology.clicked.connect(self.toggle_topology_group)
+        vbox2.addWidget(self.btn_topology)
 
         # Create a list of layer names
         self.init_ready = True      # all widgets are defined
@@ -273,21 +336,35 @@ class mmv_playground(QWidget):
         for layer in self.viewer.layers:
             layer.events.name.connect(self.find_layers)
 
-    def toggle_intensity_group(self, checked: bool):
-        # Switching the visibility of the intensity widget
+    def toggle_intensity_normalization(self, checked: bool):
+        # Switching the visibility of the intensity normalization group
         # (15.11.2024)
-        if self.intensity_group.isVisible():
-            self.intensity_group.setVisible(False)
+        if self.intensity_normalization.isVisible():
+            self.intensity_normalization.setVisible(False)
+            self.btn_intensity.setText('Intensity normalization')
         else:
-            self.intensity_group.setVisible(True)
+            self.intensity_normalization.setVisible(True)
+            self.btn_intensity.setText('Hide intensity normalization')
 
-    def toggle_smoothing_group(self, checked: bool):
-        # Switching the visibility of the smoothing widget
+    def toggle_smoothing(self, checked: bool):
+        # Switching the visibility of the smoothing group
         # (15.11.2024)
-        if self.smoothing_group.isVisible():
-            self.smoothing_group.setVisible(False)
+        if self.smoothing.isVisible():
+            self.smoothing.setVisible(False)
+            self.btn_smoothing.setText('Smoothing')
         else:
-            self.smoothing_group.setVisible(True)
+            self.smoothing.setVisible(True)
+            self.btn_smoothing.setText('Hide smoothing')
+
+    def toggle_background_correction(self, checked: bool):
+        # Switching the visibility of the background correction group
+        # (28.11.2024)
+        if self.background_correction.isVisible():
+            self.background_correction.setVisible(False)
+            self.btn_background.setText('Background correction')
+        else:
+            self.background_correction.setVisible(True)
+            self.btn_background.setText('Hide background correction')
 
     def find_layers(self, event: napari.utils.events.event.Event):
         # (19.11.2024)
@@ -298,10 +375,12 @@ class mmv_playground(QWidget):
         self.layer_names = lst
 
         if self.init_ready:
-            self.intensity_group.cbx_image.clear()
-            self.intensity_group.cbx_image.addItems(lst)
-            self.smoothing_group.cbx_image.clear()
-            self.smoothing_group.cbx_image.addItems(lst)
+            self.intensity_normalization.cbx_image.clear()
+            self.intensity_normalization.cbx_image.addItems(lst)
+            self.smoothing.cbx_image.clear()
+            self.smoothing.cbx_image.addItems(lst)
+            self.background_correction.cbx_image.clear()
+            self.background_correction.cbx_image.addItems(lst)
 
     def connect_rename(self, event: napari.utils.events.event.Event):
         # (20.11.2024)
